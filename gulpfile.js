498b79ffb9558c8a59f99798a9dbe5b2c04d07c5
AJ Austinson <aj.austinson@gmail.com>
2020-10-09 14:44:48 -0700

    Got in the zone & forgot to commit. Apologies.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,15 @@ var uglify = require('gulp-uglify');
 var mustache = require('gulp-mustache');
 var fs = require('fs');
 
+/*
+	AJ (10/9/2020):
+	In general here's a list of improvements that could be made over time to the build process
+	- Splitting up CSS more granularly, right now it's just home & episodes but they share details
+	- Make things async. I feel this is low priority, it runs plenty fast as-is. If we end up with 10+ episodes let's consider it.
+	- Less hard coded values, or just organize them into one place.
+	- There's also some duplicated HTML across pages, it would be cool use mustache to pull in a stupid-simple component.
+*/
+
 // Minify & strip comments from all css in /src and put it in /build
 gulp.task('css', () => {
 	return gulp.src('src/*.css')
@@ -27,65 +36,77 @@ gulp.task('js', () => {
 		.pipe(gulp.dest('build'));
 }); 
 
-// TODO(AJ): These two tasks are *really* similar, there should be a way to clean this up
-
-// Using the templates/preview.mustache template, render out build/index.html
-// NOTE: Then run `serve build` (or use whatever local hosting solution you like) to preview things
-gulp.task('buildPreview', () => {
-	return gulp.src('templates/preview.mustache')
-		.pipe(mustache({ 
-			style: fs.readFileSync('build/home.css').toString(),
-			content: fs.readFileSync('build/home.html').toString(), 
-			script: fs.readFileSync('build/home.js').toString()
-		}))
-		.pipe(rename('index.html'))
-		.pipe(gulp.dest('build')); 
-});
-
-// Using the templates/export.mustache template, render out build.export.html
-// This is the giant blob of text that needs to be copy/pasted into hybris
-gulp.task('buildExport', () => {
-	return gulp.src('templates/export.mustache')
-		.pipe(mustache({ 
-			style: fs.readFileSync('build/home.css').toString(),
-			content: fs.readFileSync('build/home.html').toString(), 
-			script: fs.readFileSync('build/home.js').toString()
-		}))
-		.pipe(rename('export.html'))
-		.pipe(gulp.dest('build')); 
-});
-
-// TODO: This should be expanded to build all available episodes
-gulp.task('buildEpisodePreview', () => {
-	return gulp.src('templates/preview.mustache')
-		.pipe(mustache({ 
-			style: fs.readFileSync('build/episodes.css').toString(),
-			content: fs.readFileSync('build/episode1.html').toString(), 
-			script: fs.readFileSync('build/home.js').toString()
-		}))
-		.pipe(rename('episode1.html'))
-		.pipe(gulp.dest('build')); 
-});
-
-// Using the templates/export.mustache template, render out build.export.html
-// This is the giant blob of text that needs to be copy/pasted into hybris
-gulp.task('buildEpisodeExport', () => {
-	return gulp.src('templates/export.mustache')
-		.pipe(mustache({ 
-			style: fs.readFileSync('build/episodes.css').toString(),
-			content: fs.readFileSync('build/episode1.html').toString(), 
-			script: fs.readFileSync('build/home.js').toString()
-		}))
-		.pipe(rename('export_episode1.html'))
-		.pipe(gulp.dest('build')); 
-});
-
 // Just simplifing the minify tasks into one task
 gulp.task('minify', gulp.parallel('html', 'css', 'js'));
 
+// These two arrays keep track of the build task names for preview(local) & export(hybris)
+const previewBuildTasks = [], exportBuildTasks = [];
+
+// This is a helper function to create two build tasks whenever we have a new page
+// IMPROVEMENT(AJ): Right now it was built quickly (sync instead of async, hardcoded things, etc.) This could be better.
+// TODO(AJ)[BUG]: If an empty string or no value is passed for style, content, or script this task will fail
+const createBuildTask = (name, options) => { 
+	previewBuildTasks.push(name + "Preview");
+	gulp.task(name + "Preview", () => { 
+		return gulp.src("templates/preview.mustache")
+			.pipe(mustache({
+				style: fs.readFileSync("build/" + options.cssFilename).toString(),
+				content: fs.readFileSync("build/" + options.htmlFilename).toString(),
+				script: fs.readFileSync("build/" + options.scriptFilename).toString(),
+			}))
+			.pipe(rename(options.previewFilename))
+			.pipe(gulp.dest("build"));
+	});
+
+	exportBuildTasks.push(name + "Export");
+	gulp.task(name + "Export", () => { 
+		return gulp.src("templates/export.mustache")
+			.pipe(mustache({
+				style: fs.readFileSync("build/" + options.cssFilename).toString(),
+				content: fs.readFileSync("build/" + options.htmlFilename).toString(),
+				script: fs.readFileSync("build/" + options.scriptFilename).toString(),
+			}))
+			.pipe(rename(options.exportFilename))
+			.pipe(gulp.dest("build"));
+	});
+};
+
+// ----------------------------------------------------------------
+// Home page build tasks
+// ----------------------------------------------------------------
+
+createBuildTask('buildHome', { 
+	cssFilename: "home.css",			// relative to src/ that should be used as css
+	htmlFilename: "home.html",			// relative to src/ HTML only, no CSS or JS (otherwise htmlmin() doesn't work correctly)
+	scriptFilename: "home.js",			// relative to src/ for JS, just include raw JS, no need for a <script> tag
+	previewFilename: "index.html",		// relative to build/ where to put the preview html file to be viewed locally
+	exportFilename: "hybris_home.html"	// relative to build/ where to put the blob of html/css/js to be copy/pasted into Hybris
+});
+
+// ----------------------------------------------------------------
+// Episode pages build tasks
+// ----------------------------------------------------------------
+
+createBuildTask('buildEpisode1', { 
+	cssFilename: "episodes.css",
+	htmlFilename: "episode1.html",
+	scriptFilename: "home.js",
+	previewFilename: "01AliRoberto.html",
+	exportFilename: "hybris_01AliRoberto.html"
+}); 
+
+createBuildTask('buildEpisode2', { 
+	cssFilename: "episodes.css",
+	htmlFilename: "episode2.html",
+	scriptFilename: "home.js",
+	previewFilename: "02GabrielGomez.html",
+	exportFilename: "hybris_02GabrielGomez.html"
+}); 
+
+
 // These are the two tasks that should be called
 // By default, just running `gulp` will build a preview
-gulp.task('default', gulp.series('minify', 'buildPreview', 'buildEpisodePreview')); 
+gulp.task('default', gulp.series('minify', gulp.parallel(previewBuildTasks))); 
 
 // Running `gulp export` will create build/export, which is the blob to be copy/pasted into Hybris
-gulp.task('export', gulp.series('minify', 'buildExport', 'buildEpisodeExport')); 
+gulp.task('export', gulp.series('minify', gulp.parallel(exportBuildTasks))); 
--- src/home.js
+++ src/home.js
@@ -9,11 +9,20 @@ function ready(cb) {
 ready(function(){ 
 	document.querySelectorAll('.fm-video-thumbnail').forEach(function(a){ 
 		a.addEventListener('mouseover', function(e){ 
-			console.log("hi");
 			a.querySelector('iframe').contentWindow.postMessage({ method: 'play'}, '*'); 
 		});
 		a.addEventListener('mouseleave', function(e){
 			a.querySelector('iframe').contentWindow.postMessage({ method: 'pause'}, '*'); 
 		}); 
 	}); 
+
+	// NOTE: This is for debugging on Hybris. Since when connected over VPN Vimeo won't load videos
+	// so you have to disconnect from VPN and then reload all iframes. This is just a handy shortcut. 
+	window.addEventListener('keydown', function(e){ 
+		if(e.key == 'r') { 
+			document.querySelectorAll("iframe").forEach(function(iframe){
+				iframe.src = iframe.src;
+			});
+		}
+	});
 });
