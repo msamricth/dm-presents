9e613376c5ca88cd7d703597f1a33b2c393f6c8d
Emmelia Talarico <emm.talarico@gmail.com>
2022-06-03 13:47:03 -0400

    Juneteeth 2021
    New landing page.
    bhm-export extended to all bhm- pug files

---
+++ src/black-history-month/bhm.js
@@ -0,0 +1,119 @@
+function ready(cb) {
+	if( document.readyState != 'loading' ) {
+		cb();
+	} else {
+		document.addEventListener('DOMContentLoaded', cb);
+	}
+}
+
+function smoothstep(e1, e2, v) {
+	var val = (v - e1) / (e2 - e1);
+	val = Math.min(Math.max(val, 0.0), 1.0);
+	return val * val * (3.0 - 2.0 * val);
+}
+
+ready(function(){
+	var photoSections = document.querySelectorAll("section.bhm-photo");
+	var photoSectionsTransparent = document.querySelectorAll("section.bhm-photo-transparent");
+	var photoImagesTransparent = document.querySelectorAll(".bhm-photo-image-transparent");
+	var photoCovers = document.querySelectorAll(".bhm-photo-cover");
+	var photoFades = document.querySelectorAll(".bhm-photo-fade")
+	var photoImages = document.querySelectorAll(".bhm-photo-image")
+	var footer = document.querySelector(".bhm-last-footer");
+	var header = document.querySelector(".bhm-video");
+
+	// Play vimeo video when pressing the play button
+	var videoPlaceholders = document.querySelectorAll("img.bhm-video-placeholder");
+	for(var i = 0; i < videoPlaceholders.length; i++) {
+		videoPlaceholders[i].addEventListener("click", function(e) {
+			e.preventDefault();
+			e.target.style.display = "none";
+			e.target.previousElementSibling.style.display = "none";
+			e.target.nextElementSibling.style.display = "block";
+			e.target.nextElementSibling.querySelector("iframe").contentWindow.postMessage({method:"play"}, "*"); 
+		})
+	}
+
+	var isFooterIntersecting = false;
+	var isHeaderIntersecting = false;
+
+	var previousScrollY = 0;
+	var scrollDelta = 0;
+
+	for(var i = 0; i < photoSections.length; i++) {
+		// photoSections[i].style.backgroundImage = "url(" + photoSections[i].dataset.src + ")";
+		photoImages[i].src = photoSections[i].dataset.src;
+	}
+	for(var i = 0; i < photoSectionsTransparent.length; i++) {
+		// photoSections[i].style.backgroundImage = "url(" + photoSections[i].dataset.src + ")";
+		photoImagesTransparent[i].src = photoSectionsTransparent[i].dataset.src;
+	}
+
+	var frame = function() {
+		scrollDelta = window.scrollY - previousScrollY;
+		previousScrollY = window.scrollY;
+
+		var footerY = footer.getBoundingClientRect().y;
+		var headerY = header.getBoundingClientRect().y;
+		isHeaderIntersecting = (headerY > 0);
+		isFooterIntersecting = (footerY < window.innerHeight);
+
+		for(var i = 0; i < photoSections.length; i++) {
+			var rect = photoCovers[i].getBoundingClientRect();
+			// photoImages[i].style.left = Math.round(-window.innerWidth / 8.0) + "px";
+			if(rect.y <= 0 && rect.bottom > window.innerHeight)  {
+				photoImages[i].style.position = "fixed";
+				photoImages[i].style.top = "0";
+
+				// If iOS wasn't late to the party with background-attachment:fixed then this would work
+				// Alas, we are doing things the weird way
+				// photoSections[i].style["background-attachment"] = "fixed";
+
+			}
+			else {
+				var imageY = photoImages[i].getBoundingClientRect().y;
+				photoImages[i].style.position = "absolute";
+				// This weirdness is because of getting scroll info is messed up.
+				// If we're scrolling up we want '<', but if we're scrolling down we want '<='
+				// Without this there is a single frame flicker
+				if((scrollDelta < 0 && imageY < 0) || (scrollDelta > 0 && imageY <= 0))
+					photoImages[i].style.top = "50%";
+				else 
+					photoImages[i].style.top = "0";
+
+				// If iOS wasn't late to the party with background-attachment:fixed then this would work
+				// photoSections[i].style["background-attachment"] = "scroll";
+
+			}
+
+			// photoCovers[i].innerText = window.scrollY;
+
+
+			if(rect.y <= window.innerHeight && rect.bottom > 0) {
+				var scrollAmount = (-rect.y / window.innerHeight) * 2.0 - 1.0;
+				var fadeIn = smoothstep(0.1, 0.3, scrollAmount);
+				var fadeOut = fadeIn;//smoothstep(1.1, 1.0, scrollAmount); // leaving this in on the off chance we want to re-implement
+				// photoCovers[i].innerText = fadeIn.toString() + " :: " + fadeOut.toString() + " -- " + scrollAmount.toString();
+				photoCovers[i].style.opacity = Math.min(fadeOut, fadeIn).toString();
+				if(isHeaderIntersecting) {
+					photoImages[i].style.opacity ="1.0";
+				}
+				else if(isFooterIntersecting){
+					photoImages[i].style.opacity ="0.4";
+				}
+				else {
+					var opacity = smoothstep(0.4, -1.0, scrollAmount);
+					opacity = Math.max(0.4, opacity);
+					photoImages[i].style.opacity = opacity.toString();
+				}
+			}
+			else {
+				photoImages[i].style.opacity = "1.0";
+			}
+		}
+	}
+
+	window.addEventListener("scroll", function(){
+		this.requestAnimationFrame(frame);
+	})
+});
--- src/home.js
+++ src/home.js
@@ -23,7 +23,7 @@ ready(function(){
 		video.addEventListener('canplaythrough', setStart); 
 	}; 
 
-	var items = document.querySelectorAll(".fm-episodes-item");
+	var items = document.querySelectorAll(".fm-episode");
 	for( var i = 0; i < items.length; i++ ) {
 		var a = items[i];
 		// play/pause on hover for thumbnails
@@ -35,64 +35,137 @@ ready(function(){
 			var video = e.target.querySelector("video");
 			video.pause(); 
 		}); 
+
+		a.addEventListener('click', function(e) {
+			e.preventDefault();
+			var item  = e.currentTarget;
+			var href = item.dataset.href;
+			if(href) {
+				window.location = href;
+			}
+		});
 	};
 
 	// swap for mobile collage looping video
 	// TODO: This should be moved to an episodes.js for release2
 	var collageVideo = document.querySelector(".fm-hero-video:not(.preview) video");
-	var collageContainer = collageVideo.parentElement;
-	var collageVideoSource = collageVideo.querySelector("source");
-	var collageBreakpoint = collageVideoSource.getAttribute("data-breakpoint");
-	var collageDesktopSrc = collageVideoSource.getAttribute("src");
+	if(collageVideo) {
+		var collageContainer = collageVideo.parentElement;
+		var collageVideoSource = collageVideo.querySelector("source");
+		var collageBreakpoint = collageVideoSource.getAttribute("data-breakpoint");
+		var collageDesktopSrc = collageVideoSource.getAttribute("src");
+	}
 
 	var adjustCollageVideoPadding = function() {
-		var aspect = (collageVideo.videoHeight / collageVideo.videoWidth) * 100.0;
-		collageContainer.style.paddingBottom = aspect.toString() + "%"; 
-		collageVideo.removeEventListener("canplaythrough", adjustCollageVideoPadding);
+		if(collageVideo){
+			var aspect = (collageVideo.videoHeight / collageVideo.videoWidth) * 100.0;
+			collageContainer.style.paddingBottom = aspect.toString() + "%"; 
+			collageVideo.removeEventListener("canplaythrough", adjustCollageVideoPadding);
+		}
 	};
 
 	var adjustCollageVideo = function() { 
-		var breakpoint = parseInt(collageBreakpoint);
-		var existingSrc = collageVideoSource.getAttribute("src");
-		var newSrc;
+		if(collageVideo) {
+			var breakpoint = parseInt(collageBreakpoint);
+			var existingSrc = collageVideoSource.getAttribute("src");
+			var newSrc;
 
-		if(window.outerWidth <= breakpoint)
-			newSrc = collageDesktopSrc.replace(/(https:\/\/.+)([\.]mp4)/gm, "$1Mobile$2");
-		else
-			newSrc = collageDesktopSrc; 
-		
-		if(existingSrc != newSrc) { 
-			collageVideoSource.setAttribute("src", newSrc);
-			collageVideo.addEventListener("canplaythrough", adjustCollageVideoPadding);
-			collageVideo.load();
-		}
-		else {
-			adjustCollageVideoPadding();
-			collageVideo.addEventListener("canplaythrough", adjustCollageVideoPadding);
+			if(window.innerWidth <= breakpoint)
+				newSrc = collageDesktopSrc.replace(/(https:\/\/.+)([\.]mp4)/gm, "$1Mobile$2");
+			else
+				newSrc = collageDesktopSrc; 
+			
+			if(existingSrc != newSrc) { 
+				collageVideoSource.setAttribute("src", newSrc);
+				collageVideo.addEventListener("canplaythrough", adjustCollageVideoPadding);
+				collageVideo.load();
+			}
+			else {
+				adjustCollageVideoPadding();
+				collageVideo.addEventListener("canplaythrough", adjustCollageVideoPadding);
+			}
 		}
 	};
 
-	var episodeItems = document.querySelectorAll(".fm-episodes-item");
-	var episodeItemContainer = document.querySelector(".fm-episodes-item-container");
-	var nextArrow = document.querySelector(".fm-episodes-next");
-	var prevArrow = document.querySelector(".fm-episodes-prev"); 
+	var episodeDetails = document.querySelector(".fm-episode-details");
+	var nextArrow = document.querySelector(".fm-episodes-n");
+	var prevArrow = document.querySelector(".fm-episodes-p"); 
+	var episodeItems = [];
 
-	var videosShown = window.outerWidth > 768 ? 3 : 2;
+	var handleEpisodeSelectorClick = function(e) {
+		var selectedSeason = e.currentTarget.dataset.season;
+		if(selectedSeason !== season) {
+			setSeason(selectedSeason);
+		}
+	};
+
+	var episodeSelectors = document.querySelectorAll(".fm-episodes-selector h2")
+	for(var i = 0; i < episodeSelectors.length; i++) {
+		episodeSelectors[i].addEventListener("click", handleEpisodeSelectorClick); 
+	}
+
+	var episodeItemContainer = null;
+	function setSeason(_season) {
+		season = _season;
+
+		// hide old container
+		var episodeItemContainers = document.querySelectorAll(".fm-episodes-hug");
+		for(var i = 0; i < episodeItemContainers.length; i++){
+			episodeItemContainers[i].style.display = "none";
+		}
+
+		episodeItemContainer = document.querySelector(".fm-episodes-hug[data-season='" + season + "']");
+		episodeItems = episodeItemContainer.querySelectorAll(".fm-episode");
+		realEpisodeItems = [];
+		for(var i = 0; i < episodeItems.length; i++) {
+			if(episodeItems[i].querySelector("video"))
+				realEpisodeItems.push(episodeItems[i]);
+		}
+		episodeItems = realEpisodeItems;
+		episodeCount = episodeItems.length;
+
+		// show new container
+		episodeItemContainer.style.display = "inline-flex";
+
+		var seasonLinks = document.querySelectorAll(".fm-episodes-selector h2");
+		for(var i = 0; i < seasonLinks.length; i++) {
+			var linkSeason = seasonLinks[i].dataset.season;
+			if(linkSeason === season) 
+				seasonLinks[i].classList.add("active");
+			else 
+				seasonLinks[i].classList.remove("active");
+		}
+
+		episodeIndex = 0;
+
+		handleResize();
+	};
+
+
+	// Attempt to get the season for this page
+	var season = "2";
+	if(episodeDetails && episodeDetails.dataset.season) {
+		season = episodeDetails.dataset.season;
+	}
 
 	// Scroll to end, disabled for now
 	//var episodeIndex = episodeItems.length - videosShown;
 	var episodeIndex = 0;
-	if(window.outerWidth <= 480)
+	if(window.innerWidth <= 480)
 		episodeIndex = 0;
 
-	var episodeCount = episodeItems.length;
+	var episodeCount = 0;
 
-	var moveEpisodeCarousel = function(direction) { 
-		var videosShown = window.outerWidth > 768 ? 3 : 2;
+	var videosShown = window.innerWidth > 768 ? 3 : 2;
+
+
+
+	function moveEpisodeCarousel(direction) { 
+		var videosShown = window.innerWidth > 768 ? 3 : 2;
 		if(direction < 0)
 			episodeIndex = Math.max(0, episodeIndex - videosShown); 
 		else
-			episodeIndex = Math.min(episodeCount - videosShown, episodeIndex + videosShown);
+			episodeIndex = Math.min(episodeItems.length - videosShown, episodeIndex + videosShown);
 
 		adjustEpisodeCarouselArrows(250);
 	};
@@ -107,9 +180,9 @@ ready(function(){
 		moveEpisodeCarousel(1);
 	}); 
 
-	var adjustEpisodeCarouselArrows = function(delay) { 
+	function adjustEpisodeCarouselArrows(delay) { 
 		delay = delay | 0;
-		if( window.outerWidth <= 480 ) {
+		if( window.innerWidth <= 480 || episodeCount <= videosShown ) {
 			nextArrow.style.display = "none";
 			prevArrow.style.display = "none";
 			episodeItemContainer.style.left = "0"; 
@@ -118,18 +191,18 @@ ready(function(){
 			var mo = 20 * (episodeIndex); // margin offset, to account for the 20px margin in between each item
 			if(episodeIndex == 0) mo = 0; // ignore if we're at the first item, otherwise the math is confused
 
-			if(window.outerWidth > 1440)
+			if(window.innerWidth > 1440)
 				episodeItemContainer.style.left = parseInt(-episodeIndex * 390 - mo).toString() + "px";
-			else if(window.outerWidth > 768)
+			else if(window.innerWidth > 768)
 				episodeItemContainer.style.left = "calc((33.33vw - 46px) * " + (-episodeIndex).toString() + " - " + mo + "px)";
-			else if(window.outerWidth > 480)
+			else if(window.innerWidth > 480)
 				episodeItemContainer.style.left = "calc((50vw - 50px) * " + (-episodeIndex).toString() + " - " + mo + "px)"; 
 
 			episodeItemContainer.style.transition = "left ease-in-out 0.25s";
 
 			setTimeout(function() {
 				episodeItemContainer.style.transition = "none";
-				var videosShown = window.outerWidth > 768 ? 3 : 2;
+				var videosShown = window.innerWidth > 768 ? 3 : 2;
 				var rect = episodeItems[episodeIndex].querySelector("video").getBoundingClientRect(); 
 				var lastIndex = Math.min(episodeCount-1, episodeIndex+(videosShown-1));
 				var endRect = episodeItems[lastIndex].querySelector("video").getBoundingClientRect(); 
@@ -148,17 +221,18 @@ ready(function(){
 
 	};
 
-	var handleResize = function(e) {
+	function handleResize(e) {
 		if( collageVideo !== null ) 
 			adjustCollageVideo();
 		
 		adjustEpisodeCarouselArrows(); 
 	}; 
 
-	handleResize();
 	window.addEventListener('resize', function(e){ handleResize(); });
 	window.addEventListener('orientationchange', function(e){ handleResize(); });
 	window.addEventListener("scroll", adjustEpisodeCarouselArrows );
+	
+	setSeason(season);
 
 	var episodePlaceholderTeaser = document.querySelector(".fm-episode-header-video");
 	if(episodePlaceholderTeaser !== null) {
